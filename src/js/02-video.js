5ef7b7776c7d1f95635ac780746fd17b094fbce2
11Alex111 <xalexey.g@gmail.com>
2023-08-26 22:28:23 +0300

    02-video update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveCurrentTime = throttle((currentTime) => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', saveCurrentTime);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime).catch((error) => {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
 
